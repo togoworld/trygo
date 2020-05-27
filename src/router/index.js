d798cdcdc26fbc9ed7dd9bf04c984b748018cb4a
togoworld <59427625+togoworld@users.noreply.github.com>
2020-05-27 09:12:40 +0800

    登录和layout页面路由整合

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,22 @@
 import Vue from 'vue'
 //导入路由
 import VueRouter from 'vue-router'
-
 //注册路由
 Vue.use(VueRouter)
 
+//导入组件
+import login from '@/views/login'
+import layout from '@/views/layout'
+
 //创建路由规则
-const routes = [];
+const routes = [
+    //路由重定向
+    {path:'*',redirect:'/login'},
+    //login
+    { path: '/login', component: login },
+    //layout
+    {path:'layout',component:layout}
+];
 
 //创建路由对象
 const router = new VueRouter({
