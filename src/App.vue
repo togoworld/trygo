e5d034d2909e5f54521490a271fbc9df96361c07
togoworld <59427625+togoworld@users.noreply.github.com>
2020-05-27 08:34:10 +0800

    elementUI整合

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-   
+   <router-view></router-view>
   </div>
 </template>
 
@@ -9,9 +9,6 @@
 
 export default {
   name: 'App',
-  components: {
-    
-  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 //导入路由
 import router from '@/router'
+//导入elementUI并注册
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI)
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  //挂载
   router,
   render: h => h(App),
 }).$mount('#app')
